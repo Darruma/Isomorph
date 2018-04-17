bca146f06de64279561956b3747a2c2f4e0a59a3
Darruma <30578602+Darruma@users.noreply.github.com>
2018-04-17 14:36:36 -0700

    Add files via upload

---
+++ Assets/Scripts/StartScreen.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartScreen : MonoBehaviour {
+
+	public Vector3 centerPosition;
+	public GameObject Is;
+	void Start () {
+		
+	}
+	
+
+	void Update () {
+		Is.transform.position = Vector3.Lerp (Is.transform.position, centerPosition, Time.deltaTime * 2);
+	}
+
+	public void startProject()
+	{
+		Application.LoadLevel (1);
+	}
+}
